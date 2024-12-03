2a8ad31c7a43beb6555b37006eef80f0573515d8
rokrajher <69865825+rokrajher@users.noreply.github.com>
2024-12-03 14:57:05 +0100

    Update footer.tsx

--- frontend/app/components/footer.tsx
+++ frontend/app/components/footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className='bg-sky-800 text-neutral-300 text-center p-5'>
+    <footer className='bg-sky-800 text-neutral-300 text-center p-5 h-36 flex justify-center items-center'>
       <p>© 2024 Rok Rajher</p>
     </footer>
   );
